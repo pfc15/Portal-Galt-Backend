e3aa8c6d922ef6c61ea863a8a8c63315b1d14b4a
pedro cruz <pedrofcrux@gmail.com>
2025-01-06 14:28:11 -0300

    signup views

--- portalGalt/cadastro/views.py
+++ portalGalt/cadastro/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .serializers import UserSerializer
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+
 
 @api_view(['POST'])
 def login(request):
@@ -9,7 +14,16 @@ def login(request):
 
 @api_view(['POST'])
 def signup(request):
-    return Response({})
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        user = User.objects.get(username=request.data['username'])
+        user.set_password(request.data['password'])
+        user.save()
+        token = Token.objects.create(user=user)
+        return Response({"token":token.key, "user":serializer.data})
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 def test_token(request):

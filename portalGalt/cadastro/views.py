805b5b9c0e9f6198bc8d9d86467396afb9f956f4
pedro cruz <pedrofcrux@gmail.com>
2025-01-06 14:39:34 -0300

    login

--- portalGalt/cadastro/views.py
+++ portalGalt/cadastro/views.py
@@ -7,10 +7,16 @@ from rest_framework import status
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 
+from django.shortcuts import get_object_or_404
 
 @api_view(['POST'])
 def login(request):
-    return Response({})
+    user = get_object_or_404(User, username=request.data["username"])
+    if not user.check_password(request.data['password']):
+        return Response({"detail": "Not found"}, status=status.HTTP_400_BAD_REQUEST)
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(instance=user)
+    return Response({"token":token.key, "user":serializer.data})
 
 @api_view(['POST'])
 def signup(request):

91339ba183efc225f061674be817614f65ab26fc
João Pedro <joaopedrosilvasousa003@gmail.com>
2025-01-10 20:16:54 -0300

    feat(#10): create login page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,47 @@
 
 export default function Home() {
   return (
-    <div className="w-full h-full flex justify-center text-black">Bem vindo ao Portal Galt
+    <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
+      <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+        <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
+          <div>
+            <img src='../assets/Galt.png'
+              className="w-32 mx-auto" />
+          </div>
+          <div className="mt-12 flex flex-col items-center">
+            <h1 className="text-2xl xl:text-3xl font-extrabold">
+              Bem vindo ao Portal Galt!
+            </h1>
+            <div className="w-full flex-1 mt-8">
+              <div className="my-12 border-b text-center">
+              </div>
+
+              <div className="mx-auto max-w-xs">
+                <input
+                  className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-[#2DC5B8] focus:bg-white"
+                  type="email" placeholder="Email" />
+                <input
+                  className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-[#2DC5B8] focus:bg-white mt-5"
+                  type="password" placeholder="Password" />
+                <button
+                  className="mt-5 tracking-wide font-semibold bg-[#2DC5B0] text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                  <svg className="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+                    <circle cx="8.5" cy="7" r="4" />
+                    <path d="M20 8v6M23 11h-6" />
+                  </svg>
+                  <span className="ml-3">
+                    Entrar
+                  </span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 bg-indigo-100 text-center hidden lg:flex">
+        </div>
+      </div>
     </div>
   );
 }

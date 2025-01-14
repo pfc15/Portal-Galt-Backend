fb9bad650ff617984f24f6df6fa7b3ab36a46b30
pedro cruz <pedrofcrux@gmail.com>
2025-01-14 11:43:56 -0300

    feat(#16): do not need custom User model

--- portalGalt/cadastro/models.py
+++ portalGalt/cadastro/models.py
@@ -1,7 +1,1 @@
 from django.db import models
-from django.contrib.auth.models import User
-
-# Create your models here.
-class User(User):
-    class Meta:
-        permissions = [("can_manage_accounts", "can manage accounts")]

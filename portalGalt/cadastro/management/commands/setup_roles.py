ee19ab4393a28d2e6df13c2a1cf34523ab558f12
pedro cruz <pedrofcrux@gmail.com>
2025-01-14 11:45:15 -0300

    fix(#16): just roles no permissions

--- portalGalt/cadastro/management/commands/setup_roles.py
+++ portalGalt/cadastro/management/commands/setup_roles.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Group, Permission
-from cadastro.models import User
+
 
 class Command(BaseCommand):
     def handle(self, *args, **kargs):
@@ -8,12 +8,6 @@ class Command(BaseCommand):
         administrator_group, created = Group.objects.get_or_create(name="Administrator")
         student_group, create = Group.objects.get_or_create(name="student")
 
-        # Assing Permission
-        manage_accounts = Permission.objects.get(codename='can_manage_accounts')
-
-
-        # add permission to group
-        administrator_group.permissions.add(manage_accounts)
-        self.stdout.write(self.style.SUCCESS('Roles and permissions have been set up successfully!'))
+        self.stdout.write(self.style.SUCCESS('Roles have been set up successfully!'))
 
 

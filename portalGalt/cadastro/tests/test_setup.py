219715d8af95bef354e28ad33368475ac4b2b2fa
pedro cruz <pedrofcrux@gmail.com>
2025-01-15 01:17:53 -0300

    feat(#16): test for login

--- portalGalt/cadastro/tests/test_setup.py
+++ portalGalt/cadastro/tests/test_setup.py
@@ -1,5 +1,6 @@
 from rest_framework.test import APITestCase
 from django.urls import reverse
+from django.contrib.auth.models import Group, User
 
 
 class TestSetUp(APITestCase):
@@ -7,11 +8,36 @@ class TestSetUp(APITestCase):
         self.signup_url = reverse('signup')
         self.login_url = reverse('login')
 
-        user_data = {
+        self.groupAdmin, create = Group.objects.get_or_create(name="Administrator")
+        self.groupStudent, create = Group.objects.get_or_create(name="student")
+
+
+        self.admin, create = User.objects.get_or_create(username="test_user_admin", email="teste@test.com", password="password")
+        self.admin.set_password("password")
+        self.admin.save()
+        self.student, create = User.objects.get_or_create(username="test_user_student", email="teste@test.com", password="password")
+        self.student.set_password("password")
+        self.student.save()
+
+        self.user_data_not_exist = {
             "email": "email@email.com",
             "username": "email",
             "password": "senha",
         }
+
+        self.user_data_admin = {
+            "email": "teste@test.com",
+            "username": "test_user_admin",
+            "password": "password",
+        }
+
+        self.user_data_student = {
+            "email": "teste@test.com",
+            "username": "test_user_student",
+            "password": "password",
+        }
+
+        
 
         return super().setUp()
     
--- portalGalt/cadastro/tests/tests_views.py
+++ portalGalt/cadastro/tests/tests_views.py
@@ -1,4 +1,8 @@
 from .test_setup import TestSetUp
+from unittest.mock import patch, MagicMock
+from django.contrib.auth.models import User, Group
+from django.test import TestCase
+import json
 
 
 class TestViews(TestSetUp):
@@ -6,4 +10,23 @@ class TestViews(TestSetUp):
     def test_user_cannot_register_with_not_data(self):
         res = self.client.post(self.signup_url)
         self.assertEqual(res.status_code, 400)
+
+
+    def test_login_sucefull(self):
+
+        header = {
+            'CONTENT_TYPE': 'application/json'
+        }
+        res = self.client.post(self.login_url, self.user_data_admin   
+        , **header)
+        
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.data["user"]["username"], self.user_data_admin["username"])
+        self.assertEqual(res.data["user"]["email"], self.user_data_admin["email"])
+
+
+        # Call the function that queries the User model
+
+
+
 

893609020eb53b7061d73902ca39aa5e5d2857c9
pedro cruz <pedrofcrux@gmail.com>
2025-01-28 17:24:08 -0300

    feat(#19): add models de presença

--- portalGalt/frequencia/admin.py
+++ portalGalt/frequencia/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Presenca
 
-# Register your models here.
+@admin.register(Presenca)
+class FrequenciaAdmin(admin.ModelAdmin):
+    list_display = ("data","presenca", 'aluno')
--- portalGalt/frequencia/models.py
+++ portalGalt/frequencia/models.py
@@ -2,10 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class presenca(models.Model):
+class Presenca(models.Model):
     data = models.DateField()
     presenca = models.IntegerField()
     aluno = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.aluno} {self.data}: {self.presenca if self.presenca>0 else "ausente"}"
 
 
 

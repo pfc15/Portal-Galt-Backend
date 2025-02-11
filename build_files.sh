2140bb7d0190582d19fcdd802bb70e278c5ca8f9
pedro cruz <pedrofcrux@gmail.com>
2025-02-10 23:57:25 -0300

    build for backend

---
+++ build_files.sh
@@ -0,0 +1,14 @@
+# Install dependencies
+python3 -m pip install -r requirements.txt
+
+# Collect static files
+python3 manage.py collectstatic --noinput
+
+# Create Vercel-compatible output vercel directory
+mkdir -p .vercel/output/static
+cp -r staticfiles/ .vercel/output/static/
+
+cd portalGalt/
+
+python3 manage.py makemigrations
+python3 manage.py migrate
